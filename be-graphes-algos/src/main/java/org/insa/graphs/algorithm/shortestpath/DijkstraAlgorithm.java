8c9219866dc97aa7031cd982056fe3eee35c043c
Clémentine Bonneau <Clementine@macbook-pro-de-clementine.home>
2020-04-24 10:48:08 +0200

    Dijkstra en cours

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,16 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.algorithm.ArcInspector;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+
+
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,8 +21,63 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        Graph graph = data.getGraph();
+        List<Node> nodes = graph.getNodes();
+        
+        BinaryHeap<Label> heap = new BinaryHeap<Label>();
+        Label[] labels = new Label[graph.size()];
+        
+        Node origine = data.getOrigin();
+        
+        
+        // initialisation de tous les labels
+        for (Node node : nodes ) {
+        		labels[node.getId()] = new Label(node,false,Float.POSITIVE_INFINITY,null);
+        }
+        
+        labels[origine.getId()].setCost(0);
+        heap.insert(labels[origine.getId()]);
+        
+        while(!heap.isEmpty()) {
+        	Label label_courrant = heap.deleteMin();
+        	label_courrant.marquer();
+        	Node noeud_courant = label_courrant.getNode();
+        	
+        	for (Arc arc : noeud_courant.getSuccessors()) {
+        		Node noeud_destination = arc.getDestination();
+        		Label label_destination = labels[noeud_destination.getId()];
+        		
+        		if (!label_destination.isMarque()) {
+        			double cout = label_courrant.getCost() + data.getCost(arc);
+        			label_destination.setCost(cout);
+        			label_destination.setPere(arc);
+        			heap.insert(label_destination);
+        		}
+        		else {
+        			
+        		}
+        			
+        	}
+        	
+       /* 	
+        	
+        	pour chaque successeur :
+        		if jamais rencontré (cout infini) : calcul cout, mis a jour label, ajout tas, parent = curent node
+        		if deja rencontré : comparer ancien cout et nouveau cout
+        			si moins bon : rien
+        			sinon : supp dans le tas puis modifier et remettre dans le tas */
+        		
+        }
+        
+        
         return solution;
     }
 
 }
+
+//Noeud initial
+// fil dans binaryheap
+//cherche plus petit
+// le petit redevient le noeud initial
+// On l'enleve après
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,26 +1,56 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
 import org.insa.graphs.model.Node;
 
-public class Label{
+
+public class Label implements Comparable<Label>{
 	
 	private Node sommet_courant;
 	private boolean marque;
-	private float cout;
+	private double cout;
 	private Arc pere;
 	
 	
-	public Label(Node a, boolean m,  Arc p, float c) {
+	public Label(Node a, boolean m, double c,  Arc p) {
 		this.sommet_courant = a;
 		this.marque = m;
 		this.cout = c;
 		this.pere =p;
 	}
 	
+	public void setPere(Arc p) {
+		this.pere = p;
+	}
 	
-	public float getCost() {
+	public void marquer() {
+		this.marque = true;
+	}
+	
+	public boolean isMarque() {
+		return this.marque;
+	}
+	
+	
+	public double getCost() {
 		return this.cout;
+	}
+	
+	public void setCost(double cost) {
+		this.cout = cost;
+	}
+	
+	public int compareTo(Label other) {
+        return Float.compare(getCost(), other.getCost());
+    }
+	
+	public Node getNode() {
+		return this.sommet_courant;
 	}
 	
 }
